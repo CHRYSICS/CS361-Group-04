a5f13612fffce5f510a68e2f763d916a666a1b92
Elaine Laguerta <12928553+elaguerta@users.noreply.github.com>
2020-11-05 16:56:48 -0800

    Fix syntax errors in schema.sql

--- example/flaskr/schema.sql
+++ example/flaskr/schema.sql
@@ -2,23 +2,23 @@
 -- Drop any existing data and create empty tables.
 
 DROP TABLE IF EXISTS ingredient;
-DROP TABLE IF EXISTS ingredientCat;
+DROP TABLE IF EXISTS ingredient_cat;
+
+CREATE TABLE ingredient_cat (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  name TEXT UNIQUE NOT NULL
+);
 
 CREATE TABLE ingredient (
   id INTEGER PRIMARY KEY AUTOINCREMENT,
   name TEXT UNIQUE NOT NULL,
-  FOREIGN KEY (category_id) REFERENCES ingredientCat (id),
-  rNourishment REAL DEFAULT 0,
-  rValue REAL DEFAULT 0,
-  rHumanWelfare REAL DEFAULT 0,
-  rAnimalWelfare REAL DEFAULT 0,
-  rResourceCons REAL DEFAULT 0,
-  rBiodiversity REAL DEFAULT 0,
-  rGlobalWarming REAL DEFAULT 0,
+  category_id INTEGER NOT NULL,
+  r_nourishment REAL DEFAULT 0,
+  r_value REAL DEFAULT 0,
+  r_human_welfare REAL DEFAULT 0,
+  r_animal_welfare REAL DEFAULT 0,
+  r_resource_cons REAL DEFAULT 0,
+  r_biodiversity REAL DEFAULT 0,
+  r_global_warming REAL DEFAULT 0,
+  FOREIGN KEY (category_id) REFERENCES ingredient_cat (id)
 );
-
-CREATE TABLE ingredientCat (
-  id INTEGER PRIMARY KEY AUTOINCREMENT,
-  name TEXT UNIQUE NOT NULL,
-);
-
