762a7167c85d51fd23d4f2af573f4c416a043059
chrysics <eckersoc@oregonstate.edu>
2020-11-08 15:12:32 -0800

    F4-02: Portal (guest or user)
    These changes redirect the application to first go through the portal for the user to decide to expore the application as a guest or user (first part of F4-02).

--- example/flaskr/__init__.py
+++ example/flaskr/__init__.py
@@ -36,15 +36,22 @@ def create_app(test_config=None):
     db.init_app(app)
 
     # apply the blueprints to the app
-    from flaskr import auth, blog
+    from flaskr import auth, blog, guest
 
+    app.register_blueprint(guest.bp)
     app.register_blueprint(auth.bp)
     app.register_blueprint(blog.bp)
 
-    # make url_for('index') == url_for('blog.index')
+    # new comment:
+    # make url_for('portal') == url_for('guest.portal')
+    # Before navigating to the content of this application,
+    # direct user to portal.  This gives user the option 
+    # to explore the application as either guest or user.
+
+    # old comment:
     # in another app, you might define a separate main index here with
     # app.route, while giving the blog blueprint a url_prefix, but for
     # the tutorial the blog will be the main index
-    app.add_url_rule("/", endpoint="index")
+    app.add_url_rule("/", endpoint="portal")
 
     return app
