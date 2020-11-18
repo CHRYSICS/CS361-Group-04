ada669e2a5384f68fc230e8e7a90d9683f724264
Elaine Laguerta <12928553+elaguerta@users.noreply.github.com>
2020-11-17 22:42:52 -0800

    Add query to get all ingredients with better avg rating

--- example/flaskr/ingredient.py
+++ example/flaskr/ingredient.py
@@ -19,29 +19,31 @@ RATINGS = [ 'r_nourishment',
             'r_biodiversity',
             'r_global_warming',]
 
-@bp.route("/<str:ingredientName>", methods=("GET", "POST"))
+@bp.route("/<ingredientName>")
 def getIngredient():
     """Retrieve an Ingredient by name
     Returns None if Ingredient not present
     """
-    if request.method == "GET":
-        db = get_db()
-        error = None
+    if request.method != "GET":
+        abort(405)
+    
+    db = get_db()
+    error = None
+    
+    if not ingredientName:
+        error = "Ingredient name is required."
+    else:
+        ingredient = db.execute(
+            "SELECT * FROM ingredient WHERE name = ?", ingredientName)
+            .fetchone()
+    
+    if ingredient is None:
+        abort(404, f"Ingredient with name {ingredientName} doesn't exist.")
+    
+    return ingredient
 
-        if not ingredientName:
-            error = "Ingredient name is required."
-        else:
-            ingredient = db.execute(
-                "SELECT * FROM ingredient WHERE name = ?", ingredientName)
-                .fetchone()
-        
-        if ingredient is None:
-            abort(404, f"Ingredient with name {ingredientName} doesn't exist.")
-        
-        return ingredient
 
-
-@bp.route("/<str:ingredientName>/alt", methods=("GET"))
+@bp.route("/<str:ingredientName>/alt")
 def getAlternatives():
     """ Retrieve a list of Ingredients that may be substituted for 
     ingredientName
@@ -85,7 +87,6 @@ def getAlternativesByRating(ratingName):
 
         return alternatives
 
-
 def getAlternativesByRatingAvg():
     """ Retrieve a list of Ingredients that may be subsituted for ingredientName 
     with a higher average of all ratings
@@ -98,10 +99,15 @@ def getAlternativesByRatingAvg():
             error = "Ingredient name is required."
         else:
             alternatives = db.execute(
-                "SELECT * FROM ingredient WHERE r_Average > "
-                "(SELECT r_Average FROM ingredient WHERE name = ?)",
+                "SELECT * FROM ingredient WHERE \
+                SELECT (r_nourishment + r_value + r_human_welfare + \
+                r_animal_welfare + r_resource_cons + r_biodiversity + \
+                r_global_warming)/7 as r_avg \
+                FROM ingredient WHERE r_avg > \
+                SELECT (r_nourishment + r_value + r_human_welfare + \
+                r_animal_welfare + r_resource_cons + r_biodiversity + \
+                r_global_warming)/7 FROM ingredient WHERE name = ?",
                 ingredientName)
-            # TODO: store the r_Average as a column in the database
 
         if alternatives is None:
             abort(
