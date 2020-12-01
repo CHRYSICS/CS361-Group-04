91f01b267d697c8bfaa55b65eb7ef052cfb7c4cc
Elaine Laguerta <12928553+elaguerta@users.noreply.github.com>
2020-11-30 16:42:44 -0800

    Check if the recipe exists, check if the recipe is already saved to the user's book

--- example/flaskr/cookbook.py
+++ example/flaskr/cookbook.py
@@ -18,17 +18,28 @@ bp = Blueprint("cookbook", __name__, url_prefix="/cookbook")
 
 
 @bp.route("/add_recipe/<int:recipe_id>", methods=("GET",))
+@login_required
 def save_recipe(recipe_id):
     """Save recipe to current user's recipe book."""
     error = None
 
     if g.user is None:
         error = "Must Be a User to Save to Recipe Book"
+    
+    db = get_db()
+    if (
+        db.execute("SELECT * FROM recipeBook_recipe WHERE \
+            recipeBook_id = (SELECT id FROM recipeBook WHERE author_id = ?)\
+                     AND recipe_id = ?",
+                   (g.user["id"],recipe_id)).fetchone()
+        is not None
+    ):
+        error = f"This recipe is already saved to your Recipe Book."
+
     if error is not None:
         flash(error, "error")
     else:
-        db = get_db()
-
+        get_recipe(recipe_id)
         db.execute(
             "INSERT INTO recipeBook_recipe (recipeBook_id, recipe_id) \
             VALUES ( (SELECT id FROM recipeBook WHERE author_id = ?), ?)", 
@@ -41,16 +52,29 @@ def save_recipe(recipe_id):
     return redirect(url_for("recipe.index"))
 
 
-@bp.route("/<int:id>/delete", methods=("POST",))
+@bp.route("/delete_recipe/<int:recipe_id>", methods=("GET",))
 @login_required
-def delete(id):
-    """Delete a recipe from cookbook.
-
-    Ensures that the recipe exists and that the logged in user is the
-    author of the recipe.
+def delete(recipe_id):
+    """Delete a recipe from the logged in user's cookbook.
     """
-    get_recipe(id)
-    db = get_db()
-    db.execute("DELETE FROM recipe WHERE id = ?", (id,))
-    db.commit()
+    error = None
+
+    if g.user is None:
+        error = "Must Be a User to Delete From Recipe Book"
+    if error is not None:
+        flash(error, "error")
+    else:
+        get_recipe(recipe_id)
+        db = get_db()
+        db.execute(
+            "DELETE FROM recipeBook_recipe \
+            WHERE recipeBook_id = \
+            (SELECT id FROM recipeBook WHERE author_id = ?) AND recipe_id = ?)",
+            (g.user["id"], recipe_id,)
+        ).fetch_one()
+
+        db.commit()
+        flash(error, "Added to recipe book!")
+        return redirect(url_for("recipe.index"))
+
     return redirect(url_for("recipe.index"))
